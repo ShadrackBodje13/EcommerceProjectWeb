d1761563f83d4a33e7dfc9cc8927d12ca03bc387
ShadrackBodje13 <56391856+ShadrackBodje13@users.noreply.github.com>
2021-01-21 18:19:28 +0100

    Update databaseSeeder.php

--- EcommerceProjectWeb/database/seeders/DatabaseSeeder.php
+++ EcommerceProjectWeb/database/seeders/DatabaseSeeder.php
@@ -3,8 +3,10 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User as User;
 
 class DatabaseSeeder extends Seeder
+
 {
     /**
      * Seed the application's database.
@@ -14,5 +16,14 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $user = new User;
+        $user->firstname = 'Shadrack';
+        $user->lastname = 'Bodje';
+        $user->birthday = '1999-01-13';
+        $user->email = 'shadrackemmanuel.bodje@ynov.com';
+        $user->password = bcrypt('Ynov12345');
+        $user->solde = '1500';
+        $user->save(); //pour le sauvegarder dans la base de données
+        
     }
 }
